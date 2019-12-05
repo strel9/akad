89bbd88ebb0b4d56f6d1eca82f06c7ede1f50d08
strel9 <strel9@gmail.com>
2019-12-05 17:40:13 +0200

    js changes

---
+++ main.js
@@ -0,0 +1,6 @@
+// let bread3 = '';
+let benefitRed = document.getElementsByClassName('ba-benefit__title');
+// let team = '';
+benefitRed[0].style.color = 'red';
+benefitRed[1].style.color = 'red';
+benefitRed[2].style.color = 'red';
